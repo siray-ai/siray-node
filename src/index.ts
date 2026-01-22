2aca6cdb94883654169856c54f04adb610c19fde
dev.siray <dev.siray@gmail.com>
2026-01-22 22:21:11 +0800

    feat: export File class from main entry point

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { Siray } from './client';
 import { Image } from './image';
 import { Video } from './video';
+import { File } from './file';
 import { GenerationResponseImpl, TaskStatusImpl } from './task-utils';
 
 // Export all classes and types
 export { Siray };
 export { Image };
 export { Video };
+export { File };
 export { GenerationResponseImpl, TaskStatusImpl };
 
 // Export types
